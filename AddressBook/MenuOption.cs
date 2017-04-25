28acc3e3ef931333607e131556d9488b233b4951
Bryan Rosenberg <Bryan.Rosenberg@bcbsnc.com>
2017-04-25 12:15:13 -0400

    Added an enum

---
+++ AddressBook/MenuOption.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AddressBook
+{
+    public enum MenuOption
+    {
+        Exit,
+        AddPerson,
+        AddCompany,
+        ListContacts,
+        SearchContacts,
+        RemoveContact
+    }
+}
