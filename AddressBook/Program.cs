803df4531b5dbac813be3c33746266b205372984
Bryan Rosenberg <Bryan.Rosenberg@bcbsnc.com>
2017-04-26 16:02:12 -0400

    Commit with a comment

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -7,6 +7,7 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
+            // I Love This Stuff
             string name = ConfigurationManager.AppSettings["ApplicationName"];
             Console.WriteLine("Welcome to: ");
             Console.WriteLine(name);
